23c740bbb55a5a4a5afb6862039e9b90df11cd32
kazuma0235 <20j70002ak@edu.tca.ac.jp>
2020-07-14 15:36:18 +0900

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,13 +6,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            double height = 1.7; 
-            double weight = 45; 
-            double bmi;
+            float height = GetInputAndParsedFloat("身長を入力してください");
+            float weight = GetInputAndParsedFloat("体重を入力してください");
+            float bmi = CalcBMI(weight, height);
             bmi = weight / (height * height);
             Console.WriteLine("BMIは" + bmi + "です。");
 
 
+        }
+        static float GetInputParseFloat(string log)
+        {
+            bool parseSuccess = false;
+            float parsedValue = 0f;
+            while(!parseSuccess)
+            {
+                Console.WriteLine(log);
+                string Input = Console.ReadLine();
+                parseSuccess = float.TryParse(Input, out parsedValue);
+            }
         }
     }
 }
